10cf75bde291af1b2155d179b03f3fdb276a54f9
James Blair <james@blair.nz>
2023-12-01 20:13:38 +1300

    draft day01

--- src/day01.zig
+++ src/day01.zig
@@ -10,7 +10,87 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day01.txt");
 
-pub fn main() !void {}
+pub fn partOne() !u32 {
+    var rollingSum: u32 = 0;
+
+    var lines = splitAny(u8, data, "\n");
+    while (lines.next()) |line| {
+        var first: ?u8 = null;
+        var last: ?u8 = null;
+
+        const chars = std.mem.bytesAsSlice(u8, line);
+        for (chars) |c| {
+            if (std.ascii.isDigit(c)) {
+                if (first == null) {
+                    // subtract 0 char to convert ANSI decimal representation to real num
+                    first = c - '0';
+                } else {
+                    last = c - '0';
+                }
+            }
+        }
+
+        // some lines only have one number
+        if (last == null) {
+            last = first.?;
+        }
+
+        // multiply by 10 to put first in tens column
+        const value = first.? * 10 + last.?;
+        rollingSum += value;
+    }
+
+    return rollingSum;
+}
+
+pub fn partTwo() !u32 {
+    const nums = [_][]const u8{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    var rollingSum: u32 = 0;
+
+    var lines = splitAny(u8, data, "\n");
+    while (lines.next()) |line| {
+        var first: ?u8 = null;
+        var last: ?u8 = null;
+
+        for (line, 0..) |c, i| {
+            // check for text num
+            for (nums, 1..) |num, j| {
+                if (std.mem.startsWith(u8, line[i..], num)) {
+                    if (first == null) {
+                        first = @intCast(j);
+                    } else {
+                        last = @intCast(j);
+                    }
+                }
+            }
+
+            // check for digit num
+            if (std.ascii.isDigit(c)) {
+                if (first == null) {
+                    first = c - '0';
+                } else {
+                    last = c - '0';
+                }
+            }
+        }
+
+        // some lines only have one number
+        if (last == null) {
+            last = first.?;
+        }
+
+        // multiply by 10 to put first in tens column
+        const value = first.? * 10 + last.?;
+        rollingSum += value;
+    }
+
+    return rollingSum;
+}
+
+pub fn main() !void {
+    print("Part 1: {}\n", .{try partOne()});
+    print("Part 2: {}\n", .{try partTwo()});
+}
 
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
